233bf73b5981a1bb237a58cb121bf9d631ad3cc0
Sri Charan Battula <charanb2005@gmail.com>
2020-05-30 21:18:31 -0700

    Chat Works

--- app/controller/main.py
+++ app/controller/main.py
@@ -2,6 +2,12 @@ from flask import (
     Blueprint, render_template
 )
 import imdb
+from flask import current_app as app
+
+
+from flask_socketio import SocketIO
+socketio = SocketIO(app._get_current_object())
+
 
 bp = Blueprint('main', __name__)
 
@@ -12,7 +18,6 @@ def index():
                            title='Flask-PWA')
 
 @bp.route('/movies')
-
 def movies():
 
     ia = imdb.IMDb()
@@ -29,3 +34,20 @@ def movies():
         movies.append(movie)
 
     return render_template('main/movies.html', movies = movies )
+
+@bp.route('/chat')
+def chat():
+    socketio.run(app._get_current_object(), '127.0.0.1', debug=True, port=5000)
+    return render_template('main/session.html')
+
+def messageReceived(methods=['GET', 'POST']):
+    with app.app_context():
+        print('Yo')
+        print('message was received!!!')
+
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    with app.app_context():
+        print('received my event: ' + str(json))
+        socketio.emit('my response', json, callback=messageReceived)
